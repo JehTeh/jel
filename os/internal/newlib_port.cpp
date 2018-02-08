2e5ee330f1f76e917be87a6f2fe3971f0810a583
Jonathan Thomson <jostth7@gmail.com>
2018-02-07 20:57:56 -0800

    Newlib alloc. ported, stubs for stl calls created

--- os/internal/newlib_port.cpp
+++ os/internal/newlib_port.cpp
@@ -46,6 +46,240 @@
 
 /** C/C++ Standard Library Headers */
 #include <cstdint>
+#include <cstring>
 /** jel Library Headers */
+#include "os/api_allocator.hpp"
+
+//For use with clang static analysis tools - these functions do not need a 'dllimport' attribute.
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winconsistent-dllimport"
+#endif
+//These libc functions require C bindings.
+#ifdef __cplusplus
+extern "C" {
+#endif
+void* malloc(size_t size);
+void* realloc(void* ptr, size_t size);
+void* calloc(size_t ptr, size_t size);
+void free(void* ptr);
+void _exit(int);
+int _getpid();
+int _stat(const char*, struct stat*);
+int _fstat(int, struct stat*);
+int _isatty(int);
+int _open(const char*, int, int);
+int _close(int);
+int _link(const char*, const char*);
+int _unlink(const char*);
+int _lseek(int, int, int);
+int _read(int, char*, int);
+int _write(int file, char *ptr, int len);
+#ifdef __cplusplus
+}
+#endif
+void* operator new(size_t size)
+{
+  return jel::os::systemAllocator->allocate(size);
+}
+
+void* operator new[](size_t size)
+{
+  return jel::os::systemAllocator->allocate(size);
+}
+
+void operator delete(void* ptr) noexcept
+{
+  jel::os::systemAllocator->deallocate(ptr);
+}
+
+void operator delete[](void* ptr) noexcept
+{
+  jel::os::systemAllocator->deallocate(ptr);
+}
+
+void* operator new(size_t size, char const*, int)
+{
+  return jel::os::systemAllocator->allocate(size);
+}
+
+void operator delete(void* ptr, char const*, int ) 
+{
+  jel::os::systemAllocator->deallocate(ptr);
+}
+
+auto mallocBase = [](size_t size)
+{
+  void* ptr = jel::os::systemAllocator->allocate(size);
+  return ptr;
+};
+
+auto reallocBase = [](void* ptr, size_t size)
+{
+  void* newPtr = malloc(size);
+  if(newPtr == nullptr) { return newPtr; }
+  std::memcpy(newPtr, ptr, size);
+  free(ptr);
+  return newPtr;
+};
+
+auto callocBase = [](size_t num, size_t size)
+{
+  void* ptr = malloc(size * num);
+  if(ptr == nullptr) { return ptr; }
+  std::memset(ptr, 0, size * num);
+  return ptr;
+};
+
+auto freeBase = [](void* ptr)
+{
+  jel::os::systemAllocator->deallocate(ptr);
+};
+
+void* malloc(size_t size)
+{
+  return mallocBase(size);
+}
+
+void* realloc(void* ptr, size_t size)
+{
+  return reallocBase(ptr, size);
+}
+
+void* calloc(size_t num, size_t size)
+{
+  return callocBase(num, size);
+}
+
+void free(void* ptr)
+{
+  return freeBase(ptr);
+}
+
+void* __wrap_malloc(size_t size)
+{
+  return mallocBase(size);
+}
+
+void* __wrap_realloc(void* ptr, size_t size)
+{
+  return reallocBase(ptr, size);
+}
+
+void* __wrap_calloc(size_t num, size_t size)
+{
+  return callocBase(num, size);
+}
+
+void __wrap_free(void* ptr)
+{
+  return freeBase(ptr);
+}
+
+void* __wrap__malloc_r(void*, size_t size)
+{
+  return mallocBase(size);
+}
+
+void* __wrap__realloc_r(void*, void* ptr, size_t size)
+{
+  return reallocBase(ptr, size);
+}
+
+void* __wrap__calloc_r(void*, size_t num, size_t size)
+{
+  return callocBase(num, size);
+}
+
+void __wrap__free_r(void*, void* ptr)
+{
+  return freeBase(ptr);
+}
+
+void __malloc_lock()
+{
+
+}
+
+void __malloc_unlock()
+{
+
+}
+
+void _exit(int)
+{
+  while(true) {};
+}
+
+int _kill(int, int)
+{
+  return -1;
+}
+
+int _getpid()
+{
+  return 1;
+}
 
 
+int _isatty(int)
+{
+  return 1;
+}
+
+int _stat(const char* name, struct stat* st)
+{
+  (void)name; (void)st;
+  return 0;
+}
+
+int _fstat(int file, struct stat* st)
+{
+  (void)file; (void)st;
+  return 0;
+}
+
+int _open(const char* name, int flags, int mode)
+{
+  (void)name; (void)flags; (void)mode;
+  return 0;
+}
+
+int _close(int file)
+{
+  (void)file;
+  return 0;
+}
+
+int _link(const char* from, const char* to)
+{
+  (void)from; (void)to;
+}
+
+int _unlink(const char* name)
+{
+  (void)name;
+  return 0;
+}
+
+int _lseek(int file, int offset, int smode)
+{
+  (void)file; (void)offset; (void)smode;
+  return 0;
+}
+
+int _read(int file, char* buffer, int len)
+{
+  (void)file; (void)buffer; (void)len;
+  return 0;
+}
+
+int _write(int file, char *ptr, int len)
+{
+  (void)file; (void)ptr; (void)len;
+  return 0;
+}
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
