6e7ea7524d3036cbb432a3401ae0d507d7e0e7df
Jonathan Thomson <jostth7@gmail.com>
2018-03-10 14:57:31 -0800

    Improved allocator API documentation

--- os/api_allocator.hpp
+++ os/api_allocator.hpp
@@ -2,6 +2,35 @@
  *  @brief Provides the interface components used for all allocators compatabile with jel.
  *
  *  @detail
+ *    The allocator API classes provide various dynamic memory related objects. These include:
+ *      -A generic heap allocator statistics interface. Heaps that implement this interface are
+ *      tracked in the global heap registry, allowing for easy access to detailed information about
+ *      the memory consumption of a given heap.
+ *      -An interface to the primary system allocator. The system allocator exposes the heap that is
+ *      used by the new/delete and malloc/free functions. Generally speaking, allocations should be
+ *      made via calls to new/delete, but because these calls are piped through the system
+ *      allocator, it is acceptable to use the SystemAllocator::systemAllocator()->allocate or
+ *      ->deallocate() calls interchangebly with memory returned by operator new() and malloc().
+ *      -An 'object pool' template class, which provides a generic threadsafe container for storing
+ *      re-usable objects. This is useful for cases where a shared resource is required between
+ *      multiple threads and is commonly grabbed and released. Instead of system allocator calls to
+ *      create and destroy new objects every time they are needed, which are considerably more
+ *      expensive, a object pool can be used. Objects are retrieved from the pool inside of an
+ *      ObjectContainer; the container class provides RAII gaurantees ensuring that objects grabbed
+ *      from the pool will be returned when the container is destroyed.
+ *      -A block allocator class. This is a specalized allocator (that is *not* inherently
+ *      threadsafe) that can be used when multiple allocations of objects in multiples of the block
+ *      size are expected. It is generally considerably faster than the system heap implementation
+ *      using a linked list; large differences between object size and block size can, however,
+ *      waste significant amounts of memory.
+ *
+ *  @todo
+ *    -Implement a specialized heap fragmentation visualizer. This will be primarily a debug tool
+ *    used to 'see' heap fragmentation (ideally via the CLI). Multiple tool implementations will
+ *    likely be required for different heap types (i.e. muli-linked list vs block allocator) but
+ *    should share a common abstracted interface that the visualizer can use to draw the in-use and
+ *    free memory, as well as provide statistics on allocated and free block distributions and
+ *    sizes.
  *
  *  @author Jonathan Thomson 
  */
@@ -50,6 +79,13 @@ namespace os
  *  When implementing the statistics interface, an allocator should insert calls to
  *  recordAllocation/Deallocation at the appropriate points. The get remaining/totalFreeSpace
  *  functions must be implemented manually. 
+ *  @note The allocator statistics are primarily designed as a debug tool, used to extend any built
+ *  in RTOS functionality. Note that because of this implementations may not always be highly
+ *  efficient or even entirely thread-safe. This is deemed acceptable, so long as generally speaking
+ *  calls to AllocatorStatisticsInterface functions produce correct results and do not interfere
+ *  with system behavior beyond the cost in time required to maintain and update the statistics. 
+ *
+ *  @todo implement begin()/end() iterators for AllocatorsTableEntry objects.
  *  */
 class AllocatorStatisticsInterface
 {
@@ -60,7 +96,9 @@ public:
    * */
   struct AllocatorsTableEntry
   {
+    /** The next allocator stats entry. A nullptr indicates the end of the list. */
     AllocatorsTableEntry* next;
+    /** A pointer to the allocator statistics. */
     AllocatorStatisticsInterface* statsIf;
   };
   /** Allocator names longer than this (including a NULL terminator) will be truncated. */
@@ -69,12 +107,26 @@ public:
   AllocatorStatisticsInterface(const char* allocatorName);
   /** Removes the allocator from the system allocators table. */
   virtual ~AllocatorStatisticsInterface() noexcept;
+  /** Returns the free space available within the allocator (in bytes). Note: This may not be the
+   * largest contiguous amount of free space, it is simply the total amount of space available. */
   virtual size_t freeSpace_Bytes() const noexcept = 0;
+  /** Returns the minimum ever (since system boot time, SteadyClock = 0) free space available within
+   * the allocator (in bytes). This is not the minimum ever contiguous free space. */
   virtual size_t minimumFreeSpace_Bytes() const noexcept = 0;
+  /** Returns the total size of the allocator (in bytes). If an allocator implementation can change
+   * its total available size, this number may change. */
   virtual size_t totalSpace_Bytes() const noexcept = 0;
+  /** Returns the number of allocations that have been made by the allocator since system boot.
+   * @TODO evaluate whether to move to uint64_t. */
   virtual size_t totalAllocations() const noexcept { return totalAllocations_; }
+  /** Returns the number of deallocations that have been made by the allocator since system boot.
+   * @TODO evaluate whether to move to uint64_t. */
   virtual size_t totalDeallocations() const noexcept { return totalDeallocations_; }
+  /** Returns a null terminated C-string that indicates the name of the allocator. */
   virtual const char* name() const noexcept { return name_; }
+  /** Provides a reference to the beginning of the linked list that includes all allocators
+   * implementing a statistics interface. The first item in this list is always the
+   * systemAllocator(). */
   static const AllocatorsTableEntry* systemAllocator() { return allocatorTableStart_; }
 protected:
   void recordAllocation() noexcept { totalAllocations_++; };
@@ -94,7 +146,11 @@ class AllocatorInterface
 {
 public:
   virtual ~AllocatorInterface() noexcept {}
+  /** Acquire some memory. On failure, either a nullptr is returned or an OOM exception may be
+   * thrown, depending on the allocator implementation. */
   virtual void* allocate(size_t size) = 0;
+  /** De-allocate some memory. ptr Must be a valid, non-null pointer returned by the allocate()
+   * function for this same allocator instance. */
   virtual void deallocate(void* ptr) = 0;
 };
 
@@ -107,6 +163,7 @@ class SystemAllocator : public AllocatorInterface, public AllocatorStatisticsInt
 public:
   SystemAllocator();
   ~SystemAllocator() {};
+  /** The system allocator does not support move or copy operations. */
   SystemAllocator(const SystemAllocator&) = delete;
   SystemAllocator(SystemAllocator&&) = delete;
   SystemAllocator& operator=(const SystemAllocator&) = delete;
@@ -117,13 +174,38 @@ public:
   size_t minimumFreeSpace_Bytes() const noexcept override final;
   size_t totalSpace_Bytes() const noexcept override final;
   static SystemAllocator* systemAllocator() { return systemAllocator_; }
-  /** The constructSystemAllocator function should only ever be called by the jel during startup,
-   * and never the application. */
+  /** The constructSystemAllocator() function should only ever be called by the jel during startup,
+   * and never the application. Repeated calls to this function will have no effect. */
   static void constructSystemAllocator() noexcept;
 private:
   static SystemAllocator* systemAllocator_;
 };
 
+/** @class ObjectPool
+ *  @brief A 'pool' of a given object that allows RAII acquisition and release of an object stored
+ *  in a container.
+ *
+ *  The ObjectPool provides a threadsafe manner of managing any generic objects. Objects are stored
+ *  internally in the pool in a queue, and when acquired, are stored in a RAII container that will
+ *  ensure the object is returned to the pool after it is no longer in use. This can be useful for
+ *  items that need to be shared across multiple threads, and when frequent
+ *  allocations/de-allocations are not desired. For example, internally all jel strings are stored
+ *  in an object pool (technically, std::unique_ptr<String> are stored) allowing shared use by the
+ *  CLI and logging threads as needed. This allows the jel to pre-allocate string buffers of an
+ *  appropriate size and not require multiple expensive system allocator calls whenever printing to
+ *  a logging buffer or using CLI functionality.
+ *  
+ *  Example of object pool use:
+ *  @code
+ *    ObjectPool<String, 10> sPool(); //Create a pool of 10 Strings
+ *    ... <Do other things>
+ *    {
+ *      auto sContainer = sPool.acquire(); //Returns a ObjectContainer storing a String.
+ *      std::sprintf(sContainer.stored().data(), "test %d",7); //Write a generic test to the string.
+ *      std::printf(sContainer.stored().c_str()); //Print the string.
+ *    } //The String automatically returns to the pool here when it falls out of scope.
+ *  @endcode
+ *  */
 template<typename ObjectT, size_t count>
 class ObjectPool
 {
@@ -153,6 +235,24 @@ public:
     std::unique_ptr<ObjectT> item_;
     ObjQ* q_;
   };
+  /** Construct a new ObjectPool. Note that the constructor accepts generic arguments, which are
+   * forwarded to each object when it is created and added to the pool. For example, using 
+   *  @code
+   *    ObjectPool<String, 10> sPool("Test123"); 
+   *  @endcode
+   *  will create a pool of 10 strings all storing "Test123". Perhaps more usefully, using
+   *  @code
+   *    ObjectPool<String, 10> sPool(127,' '); 
+   *  @endcode
+   *  will create a pool of 10 strings all at least 127 characters long, allowing up-front onetime
+   *  memory allocation if all future writes to said strings are less than 128 characters. Functions
+   *  can also be passed, so using
+   *  @code
+   *    size_t index = 0;
+   *    ObjectPool<String, 10> sPool([&](){ return std::to_string(index++); }); 
+   *  @endcode
+   *  would, for example, initialize each string with an incrementing index starting at 0.
+   * */
   template<typename ...Args>
   ObjectPool(Args&& ...args) : pool_(count)
   {
@@ -183,6 +283,17 @@ private:
   ObjQ pool_;
 };
 
+/** @class BlockAllocator
+ *  @brief The BlockAllocator is a basic implementation of a fixed size allocator. It is not
+ *    inherently threadsafe.
+ *
+ *  The BlockAllocator is recommended where multiple allocations in multiples of the blockSize_Bytes
+ *  need to be made. It is significantly faster than a dual linked-list heap, but does generally
+ *  require careful matching of block size and desired allocation size. Any multiple of the block
+ *  size is ideal (with some small, 4 byte overhead) for an allocation. 
+ *  Note that the BlockAllocator is not immune to fragmentation issues; allocations are placed in
+ *  the first found set of contiguous blocks that is large enough.
+ * */
 template<size_t blockSize_Bytes, size_t totalBlocks>
 class BlockAllocator : public AllocatorStatisticsInterface, public AllocatorInterface
 {
